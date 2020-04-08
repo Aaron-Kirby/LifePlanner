70abf4e01622b516c3731f3a44e622c66ba5189d
Aaron Kirby <Aaron_Kirby22@yahoo.com>
2020-04-07 19:05:07 -0500

    Login Screen accesses SQLite
    Created a simple username and password Login screen that says whether or not the info is correct

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,16 +1,24 @@
 package application;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController implements Initializable {
 	public LoginModel loginModel = new LoginModel();
 
 	@FXML
 	private Label isConnected;
+	@FXML
+	private TextField txtUserName;
+	@FXML
+	private TextField txtPassword;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -19,6 +27,20 @@ public class LoginController implements Initializable {
 		}
 		else {
 			isConnected.setText("Not Connected");
+		}
+	}
+
+	public void Login (ActionEvent event) {
+		try {
+			if (loginModel.isLogin(txtUserName.getText(), txtPassword.getText())) {
+				isConnected.setText("usrname and passowrd is correct");
+			}
+			else {
+				isConnected.setText("usrname and passowrd is NOT correct");
+			}
+		} catch (SQLException e) {
+			isConnected.setText("usrname and passowrd is NOT correct");
+			e.printStackTrace();
 		}
 	}
 
