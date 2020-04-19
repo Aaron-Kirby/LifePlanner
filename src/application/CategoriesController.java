af9a97e326d3ad263199b1e88cf1bda147f127c2
Aaron Kirby <Aaron_Kirby22@yahoo.com>
2020-04-19 17:36:52 -0500

    CreateCategory window made

--- src/application/CategoriesController.java
+++ src/application/CategoriesController.java
@@ -11,10 +11,15 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class CategoriesController implements Initializable{
 	Connection connection;
@@ -57,6 +62,21 @@ public class CategoriesController implements Initializable{
 			}
 		}
 
+	}
+
+	public void goToCreateCategory (ActionEvent event) {
+		try {
+			((Node)event.getSource()).getScene().getWindow().hide();
+			Stage primaryStage = new Stage();
+			FXMLLoader loader = new FXMLLoader();
+			Pane root = loader.load(getClass().getResource("/application/CreateCategory.fxml").openStream());
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+
+		}
 	}
 
 }
