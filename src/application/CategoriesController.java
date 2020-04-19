857493ba143a140c247936742e0977a65dcf58f0
Aaron Kirby <Aaron_Kirby22@yahoo.com>
2020-04-19 17:29:29 -0500

    Category Buttons
    Query databases and dynamically create Category buttons. Not sure how to link them to SceneBuilder yet

--- src/application/CategoriesController.java
+++ src/application/CategoriesController.java
@@ -1,17 +1,61 @@
 package application;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class CategoriesController implements Initializable{
+	Connection connection;
+	@FXML
+	private VBox vboxx;
+	List<Button> buttonList = new ArrayList<>();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		connection = SqliteConnection.Connector();
+		if (connection == null) {
+			System.out.println("Connection not successful");
+			System.exit(1);
+		}
+
+		PreparedStatement pstmt = null;
+		ResultSet resultSet = null;
+		String query = "SELECT Title from Categories where ID = ?";
+
+		try {
+			pstmt = connection.prepareStatement(query);
+			pstmt.setInt(1, LoginModel.getUserID());
+			resultSet = pstmt.executeQuery();
+
+			while(resultSet.next()) {
+				String title = resultSet.getString("Title");
+				buttonList.add(new Button(title));
+			}
+			vboxx.getChildren().clear();
+			vboxx.getChildren().addAll(buttonList);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				resultSet.close();
+				pstmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
--- src/application/LoginModel.java
+++ src/application/LoginModel.java
@@ -3,6 +3,8 @@ import java.sql.*;
 
 public class LoginModel {
 	Connection connection;
+	static int userID = 0;
+
 	public LoginModel () {
 		connection = SqliteConnection.Connector();
 		if (connection == null) {
@@ -20,7 +22,16 @@ public class LoginModel {
 		}
 	}
 
+	private void setUserID(int id) {
+		userID = id;
+	}
+
+	public static int getUserID() {
+		return userID;
+	}
+
 	public boolean uniqueUsername (String user, String pass) throws SQLException {
+		int id = 0;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
 		String query = "select * from Login where Username = ?";
@@ -35,11 +46,21 @@ public class LoginModel {
 			else {
 				PreparedStatement preparedStatement2 = null;
 				String insert = "INSERT INTO Login(Username, Password) VALUES(?, ?)";
+
 				try {
 					preparedStatement2 = connection.prepareStatement(insert);
 					preparedStatement2.setString(1, user);
 					preparedStatement2.setString(2, pass);
 					preparedStatement2.executeUpdate();
+
+					String idQuery = "SELECT ID from LOGIN where Username = ?";
+					PreparedStatement setID = null;
+					ResultSet forID = null;
+
+					setID = connection.prepareStatement(idQuery);
+					setID.setString(1, user);
+					forID = preparedStatement.executeQuery();
+					setUserID(forID.getInt("ID"));
 				} finally {
 					preparedStatement2.close();
 				}
@@ -65,6 +86,14 @@ public class LoginModel {
 
 			resultSet = preparedStatement.executeQuery();
 			if (resultSet.next()) {
+				String idQuery = "SELECT ID from LOGIN where Username = ?";
+				PreparedStatement setID = null;
+				ResultSet forID = null;
+
+				setID = connection.prepareStatement(idQuery);
+				setID.setString(1, user);
+				forID = preparedStatement.executeQuery();
+				setUserID(forID.getInt("ID"));
 				return true;
 			}
 			else {
